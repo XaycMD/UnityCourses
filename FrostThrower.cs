cdc006066cfd7cec8ba8849746e91a1553d209e9
XaycMD <vpetyushka@gmail.com>
2018-06-05 17:40:01 +0300

    Add files via upload

---
+++ FrostThrower.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrostThrower : Weapon
+{
+
+	// Use this for initialization
+	void Awake ()
+	{
+		Cooldown = 0.01f;
+		WeaponEffect.startLifetime = Range/WeaponEffect.startSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void Fire()
+	{
+		WeaponEffect.Play();
+		
+		//Create a ray from the current position and extending straight forward
+		Ray ray = new Ray(transform.position, transform.forward);
+		//Create a RaycastHit variable which will store information about the raycast
+		RaycastHit hit;
+
+		//If our raycast hits something...
+		if (Physics.Raycast(ray, out hit, Range, StrikeableMask))
+		{
+			//...move the lightning hit game object to the point of the hit...
+			//HitEffect.transform.position = hit.point;
+			//...and play the effect...
+			//HitEffect.Play();
+			//...then set the end point of the lightning bolt..
+			//_lightningBolt.EndPoint = hit.point;
+			//...then try to get a reference to an EnemyHealth script...
+			EnemyHealth enemyHealth = hit.collider.GetComponent<EnemyHealth>();
+			//...if the script exists...
+			if (enemyHealth != null)
+			{	
+				//...tell the enemy to take damage
+				enemyHealth.TakeDamage(Damage);
+			}
+		}
+		//Otherwise, if our raycast doesn't hit anything...
+		else
+		{
+			//...place the end of the bolt at maximum range
+			//_lightningBolt.EndPoint = ray.GetPoint(_range);
+		}
+		//Turn the lightning bolt game object on
+		//_lightningBolt.gameObject.SetActive(true);
+	}
+}
---
+++ Weapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour 
+{
+	[SerializeField]
+	public float Cooldown { get; protected set; } //The cooldown of the attack
+
+	[SerializeField]
+	protected int Damage;		//How much damage the attack does
+	[SerializeField]
+	protected float Range;		//How far the attack can shoot
+
+	protected bool Shooting;	//Whether weapon is currently shooting
+	
+	[SerializeField]
+	protected LayerMask StrikeableMask;  //Layermask that determines what the attack can hit
+	[SerializeField] 
+	protected ParticleSystem WeaponEffect;	//Particle system for visual effect of the weapon	
+	[SerializeField] 
+	protected ParticleSystem HitEffect;	//Particle system for visual effect of the weapon	
+	
+	//--------------------------------------------
+	//METHODS
+	//--------------------------------------------
+	public abstract void Fire();
+	
+}
